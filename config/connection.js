da6065f4135497085488d01aa741b1950aa9c438
Nick Pearce <nicholas.pearce0910@gmail.com>
2017-02-08 09:15:31 -0500

    edited burger file to sequelize

--- config/connection.js
+++ config/connection.js
@@ -1,26 +1,3 @@
-var mysql = require('mysql');
+var Sequelize = require('sequelize');
 
-var connection;
-// Created connection to JAWSDB
-
-if(process.env.JAWSDB_URL) {
-	connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
- connection = mysql.createConnection({
-	host: "localhost",
-	port: 3306,
-	user: "root",
-	password: "water1441",
-	database: "burgers_db"
-});
-};
-
-connection.connect(function(err) {
-	if (err) {
-		console.error('error connecting: ' + err.stack);
-		return;
-	}
-	console.log('connected as id ' + connection.threadId);
-});
-
-module.exports = connection;
+var sequelize =
--- models/burger.js
+++ models/burger.js
@@ -1,23 +1,63 @@
-var orm = require('../config/orm.js');
+var Sequelize = require('sequelize');
+var connection = require('../config/connection.js');
+
+var Burgers = sequelize.define('burgers', {
+	
+id: {
+	type: Sequelize.INTEGER,
+	allowNull: false,
+	autoIncrement: true,
+	primaryKey: true
+},
+burger_name: {
+	type: Sequelize.STRING(50),
+	allowNull: false,
+	validate: {
+		 is: ["^[a-z]+$",'i'];
+	}
+},
+devoured: {
+	type: Sequelize.BOOLEAN,
+	defaultValue: false
+},
+data: {
+	type: Sequelize.DATE,
+	default: Sequelize.literal('CURRENT_TIMESTAMP')
+}
+});
+
+
 
 var burger = {
 	all: function(cb) {
-		orm.all("burgers", function(res) {
+		Burgers.findAll().then(function(res) {
 			cb(res);
 		});
 	},
 
-	create: function(cols, vals, cb) {
-		orm.create("burgers", cols, vals, function(res) {
-			cb(res);
+	create: function(burger_name, cb) {
+		Burgers.create({
+			burger_name: burger_name,
+		}).then(function(data) {
+			cb();
+		}).catch(function(err) {
+			cb(err);
 		});
+	
 	},
 
 	update: function(objColVals, condition, cb) {
-		orm.update("burgers", objColVals, condition, function (res) {
-			cb(res);
-		});
-	}
+		Burgers.update({
+			devoured: true;
+		},
+		{
+			where: {id: objColVals}
+	}).then(function(data){
+		cb();
+	}).catch(function(err){
+
+	});
+}
 };
 
 module.exports = burger;
